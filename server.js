da7e2bc0cc48fa1ba13bff3676dd61322745e6a2
honnage <hon_da-1998@hotmail.com>
2022-06-29 13:43:12 +0700

    Database setup and Controllers

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+// routes
+const blogRoutes = require('./routes/blog')
 
 // app
 const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //   useCreatendex: true,
+    //   useFindAndModify: false,
+  })
+  .then(() => console.log("Database connected"));
 
 // middlewares
 app.use(morgan("dev"));
@@ -14,12 +28,13 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // core
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
-//routes
-app.get("/api", (req, res) => {
-  res.json({ time: Date().toString() });
-});
+// routes middlewares
+app.use('/api', blogRoutes)
+
 
 // port
 const port = process.env.PORT || 8000;
