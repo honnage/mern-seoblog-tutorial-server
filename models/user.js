4bb688c2658c9d3ac133b50b43467dec4922c617
honnage <hon_da-1998@hotmail.com>
2022-06-30 17:09:07 +0700

    User model and schema

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      requried: true,
+      max: 32,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+
+    name: {
+      type: String,
+      trim: true,
+      requried: true,
+      max: 32,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      requried: true,
+      max: 32,
+      unique: true,
+      lowercase: true,
+    },
+
+    profile: {
+      type: String,
+      requried: true,
+    },
+
+    hashed_password: {
+      type: String,
+      requried: true,
+    },
+
+    salt: String,
+
+    about: {
+      type: String,
+    },
+
+    role: {
+      type: Number,
+      trim: true,
+    },
+
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+
+    resetPaswordLink: {
+      data: String,
+      default: "",
+    },
+  },
+  { timestamp: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
