2683aff6b951eb94376b46df6c56cad6d7844631
honnage <hon_da-1998@hotmail.com>
2022-07-04 15:57:44 +0700

    User schema virtual fields and methods

--- models/user.js
+++ models/user.js
@@ -59,7 +59,39 @@ const userSchema = new mongoose.Schema(
       default: "",
     },
   },
-  { timestamp: true }
+  {
+    timestamp: true,
+  }
 );
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password; //create a temporarity variable called _password
+    this.salt = this.makeSalt(); // generate salt
+    this.hashed_password = this.encryptPassword(password); //encryptPassword
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.method = {
+  authenticate: function (plainText) {
+    return this.encryptPasswordncrt(plainText) === this.hashed_password
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto.createHmac("sh1", this.salt).update(password).digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
 
 module.exports = mongoose.model("User", userSchema);
